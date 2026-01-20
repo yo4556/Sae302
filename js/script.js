8cb86874f7f969cc63c52079515185e5c8b40f62
Yoan Deloustal <186198613+yo4556@users.noreply.github.com>
2026-01-20 14:13:52 +0100

    Adding arrow to return top

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ document.addEventListener("DOMContentLoaded", () => {
       : e.classList.remove("sticky");
   });
 });
+
+
+const progress = document.getElementById('progress');
+const radius = progress.r.baseVal.value;
+const circumference = 2 * Math.PI * radius;
+
+progress.style.strokeDasharray = circumference;
+progress.style.strokeDashoffset = circumference;
+
+window.addEventListener('scroll', () => {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+    let scrollPercent = scrollTop / scrollHeight;
+    if(scrollPercent > 1) scrollPercent = 1;
+
+    const offset = circumference * (1 - scrollPercent);
+    progress.style.strokeDashoffset = offset;
+});
