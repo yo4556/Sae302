92c9e1e5f48854eb523c7a6dc42bae25aee07267
etanLaurent <tr.etanlaurent52@gmail.com>
2026-01-21 11:22:35 +0100

    question button et title box and fix background

---
+++ end.php
@@ -0,0 +1,16 @@
+<?php
+include("autres_pages/head.php");
+?>
+<title>The end</title>
+</head>
+<body id="end">
+<?php
+include("autres_pages/menu.php");
+?>
+<main>
+
+</main>
+
+<?php
+include("autres_pages/footer.php");
+?>
--- interview.php
+++ interview.php
@@ -8,7 +8,7 @@ include("autres_pages/head.php");
 include("autres_pages/menu.php");
 ?>
 <main id="main">
-    <h1 class="center_f">Our interview</h1>
+    <h1 class="center_f">The interview</h1>
     <p class="center_f">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa
         consequatur nemo voluptas omnis doloremque odio quos soluta? Consequatur
@@ -20,6 +20,41 @@ include("autres_pages/menu.php");
     <h1 class="center_f">Listen to an Expert</h1>
     <h1 class="center_f">v</h1>
     <iframe src="https://www.youtube.com/embed/EBgzNhHLTmA?si=428Qfkt442D_F2Pa&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <section class="question">
+        <h1 class="center_f">The questions</h1>
+        <div class="question_box">
+            <img src="img/play.svg">
+            <div>
+                <h2>Music In General - Questions 1 to 5</h2>
+                <p>Stereo-music</p>
+            </div>
+            <h3>00:00</h3>
+        </div>
+        <div class="question_box">
+            <img src="img/play.svg">
+            <div>
+                <h2>Music In General - Questions 1 to 5</h2>
+                <p>Stereo-music</p>
+            </div>
+            <h3>00:00</h3>
+        </div>
+        <div class="question_box">
+            <img src="img/play.svg">
+            <div>
+                <h2>Music In General - Questions 1 to 5</h2>
+                <p>Stereo-music</p>
+            </div>
+            <h3>00:00</h3>
+        </div>
+        <div class="question_box">
+            <img src="img/play.svg">
+            <div>
+                <h2>Music In General - Questions 1 to 5</h2>
+                <p>Stereo-music</p>
+            </div>
+            <h3>00:00</h3>
+        </div>
+    </section>
 </main>
 
 <?php
