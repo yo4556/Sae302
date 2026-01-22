b89b202958e2760c2dc6449cb00858224b3cfa7a
Yoan Deloustal <186198613+yo4556@users.noreply.github.com>
2026-01-22 10:26:10 +0100

    footer

--- autres_pages/footer.php
+++ autres_pages/footer.php
@@ -1,5 +1,18 @@
     <script src="js/script.js"></script>
     <footer>
+        <div>
+            <h1>PAGES</h1>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="social.php">Social And Community</a></li>
+                <li><a href="interview.php">An Expert's View</a></li>
+                <li><a href="game.php">Behind The Sound</a></li>
+                <li><a href="credit.php">Final Note</a></li>
+            </ul>
+        </div>
+        <div>
+            <h1>Webdocumentary</h1>
+        </div>
     </footer>
 </body>
 </html>
