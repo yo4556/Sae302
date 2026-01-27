3003cebbaea8a27d291824adc111a686e8de2d99
Yoan Deloustal <186198613+yo4556@users.noreply.github.com>
2026-01-27 19:11:41 +0100

    ajout de favicon

--- autres_pages/head.php
+++ autres_pages/head.php
@@ -5,3 +5,4 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="css/style.css" />
+    <link rel="icon" type="image/svg+xml" href="img/note.svg" />
