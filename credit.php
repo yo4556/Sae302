dc2dba47e76f231229fdc22dbc926273e9ac9606
Yoan Deloustal <186198613+yo4556@users.noreply.github.com>
2026-01-27 16:32:51 +0100

    Credit finit

--- credit.php
+++ credit.php
@@ -3,7 +3,7 @@ include("autres_pages/head.php");
 ?>
 <title>Social</title>
 </head>
-<body id="credit">
+<body id="end">
 <?php
 include("autres_pages/menu.php");
 ?>
@@ -12,6 +12,21 @@ include("autres_pages/menu.php");
     <div class="disque">
         <img class="img_disque" src="img/vinyl_rose.png">
     </div>
+    <h1>Final Note</h1>
+    <p>We have previously explored how music shapes us. While genres might categorize us, emotions are universal and music taste is subjective. Next time you judge a song or a person by what he listen to, try to understand why this style is liked by people. You might be surprised with what you find, or not, and that's ok because you tried.</p>
+
+    <h3>Thank you for reading</h3>
+
+    <div id="us">
+        <img src="img/" alt="">
+        <img src="img/" alt="">
+        <img src="img/" alt="">
+        <img src="img/" alt="">
+        <img src="img/" alt="">
+    </div>
+
+    <p class="p-us">Made by<br>RICHAND Enzo - LAMOUREUX Lauren - LAURENT Etan - DELOUSTAL Yoan - Noblot Hugo</p>
+    <p class="p-us">Bibliography</p>
 
 </main>
 
